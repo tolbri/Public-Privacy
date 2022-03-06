5c39ce5784a114d144b90b8e6f2e89719988ebba
tolbri <timolbrich@icloud.com>
2022-03-06 19:59:23 +0100

    yaml integration

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const YAML = require('yaml');
+
+const handleRequest = async () => {
+  const [meta, navigation] = await Promise.all([
+    YAML.parse(fs.readFileSync('./content/meta.yml', 'utf8')),
+    YAML.parse(fs.readFileSync('./content/navigation.yml', 'utf8')),
+  ]);
+
+  return {
+    meta,
+    navigation,
+  };
+};
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+  await handleRequest();
+
   res.render('pages/home');
 });
 
